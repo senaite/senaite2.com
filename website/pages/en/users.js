4f742d264e29602b6aba739612e6f71d93de9108
Ramon Bartl <rb@ridingbytes.com>
2019-10-03 22:31:45 +0200

    Changed heading of users section

--- website/pages/en/users.js
+++ website/pages/en/users.js
@@ -27,11 +27,11 @@ class Users extends React.Component {
         <Container padding={['bottom', 'top']}>
           <div className="showcaseSection">
             <div className="prose">
-              <h1>Who is Using This?</h1>
-              <p>This project is used by many folks</p>
+              <h1>Organizations using SENAITE</h1>
+              <p>Thank You!</p>
             </div>
             <div className="logos">{showcase}</div>
-            <p>Are you using this project?</p>
+            <p>Are you using SENAITE?</p>
             <a href={editUrl} className="button">
               Add your company
             </a>
